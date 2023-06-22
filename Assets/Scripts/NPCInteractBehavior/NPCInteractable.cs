47f74d402ba95518427d8e6bd990aa61acc5b5f4
JunyeZzz <zhang.junye@northeastern.edu>
2023-06-22 17:49:13 -0400

    update

--- Assets/Scripts/NPCInteractBehavior/NPCInteractable.cs
+++ Assets/Scripts/NPCInteractBehavior/NPCInteractable.cs
@@ -17,6 +17,7 @@ public class NPCInteractable : MonoBehaviour
     public GameObject nuwaVFX;
     GameObject gate;
     bool isDestroyed = false;
+    bool isInteractable = true;
 
     public List<string> dialogues = new List<string>()
         {
@@ -29,7 +30,11 @@ public class NPCInteractable : MonoBehaviour
      // Start is called before the first frame update
     void Start()
     {   
-        gate = GameObject.FindGameObjectWithTag("Gate");
+        if(GameObject.FindGameObjectWithTag("Gate") != null)
+        {
+            gate = GameObject.FindGameObjectWithTag("Gate");
+        }
+        
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -38,38 +43,42 @@ public class NPCInteractable : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.E) && NPCIsInteracting){
             Interact();
+            
         }
     }
 
      public void Interact(){
-        player.GetComponent<PlayerInteract>().isInteracting = true;
-        player.GetComponent<PlayerInteract>().playerInteractButton.SetActive(false);
-        //player.GetComponent<CharacterController>().enabled = false;
-        if(index < dialogues.Count){
-            NewDialogue(dialogues[index]);
-            index += 1;
-        } else {
-            NPCIsInteracting = false;
-            NPCTextSystem.SetActive(false);
-            index = 0;
-            player.GetComponent<PlayerInteract>().isInteracting = false;
-            player.GetComponent<PlayerInteract>().playerInteractButton.SetActive(true);
-            //player.GetComponent<CharacterController>().enabled = true;
-            if(!isDestroyed){
-                if(gate != null)
-                {
+        if(isInteractable){
+            player.GetComponent<PlayerInteract>().isInteracting = true;
+            player.GetComponent<PlayerInteract>().playerInteractButton.SetActive(false);
+            //player.GetComponent<CharacterController>().enabled = false;
+            if(index < dialogues.Count){
+                NewDialogue(dialogues[index]);
+                index += 1;
+            } else {
+                
+                NPCIsInteracting = false;
+                
+                NPCTextSystem.SetActive(false);
+                index = 0;
+                player.GetComponent<PlayerInteract>().isInteracting = false;
+                player.GetComponent<PlayerInteract>().playerInteractButton.SetActive(true);
+                //player.GetComponent<CharacterController>().enabled = true;
+                isInteractable = false;
+                if(!isDestroyed && gate != null){
                     Destroy(gate, 0.5f);
                     Instantiate(gateVFX, gate.transform.position, gate.transform.rotation);
+                    isDestroyed = true;
                 }
-                isDestroyed = true;
+                Destroy(gameObject, 0.5f);
+                Instantiate(nuwaVFX, transform.position, transform.rotation);
             }
-            Destroy(gameObject);
-            Instantiate(nuwaVFX, transform.position, transform.rotation);
         }
+        
     }
     
     void NewDialogue(string next){
-        //Debug.Log("Start Dialogue");
+        Debug.Log("Start Dialogue");
         dialogueText.text = next;
         nameText.text = NpcName;
         NPCTextSystem.SetActive(true);
